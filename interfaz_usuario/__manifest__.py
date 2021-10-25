45cc33078a14e5c035890341878cb9f34bb721e8
Maria <maria.bardales@conectsen.com>
2021-10-25 13:47:19 +0000

    [ADD] Modulo interfaz_usuario

---
+++ interfaz_usuario/__manifest__.py
@@ -0,0 +1,30 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Interfaz Usuario Turnos',
+    
+    'sumary': """Interfaz de Usuario Turnos""",
+    
+    'description': """
+        Modulo Interfaz de usuario para administrar los turnos:
+        - Turnos
+    """,
+    
+    'autor': 'Odoo',
+    
+    'website':'http://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+       
+    ],
+}
